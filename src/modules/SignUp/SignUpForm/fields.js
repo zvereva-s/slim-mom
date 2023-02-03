8d9203dd82a0a03bfc070b6d64137bf10503d133
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-03 19:05:23 +0200

    Create fields.js

---
+++ src/modules/SignUp/SignUpForm/fields.js
@@ -0,0 +1,25 @@
+export const fields = [
+  {
+    id: 1,
+    name: "name",
+    placeholder: "Name *",
+    type: "text",
+    min: "2",
+    required: true,
+  },
+  {
+    id: 2,
+    name: "email",
+    placeholder: "Login *",
+    type: "email",
+    required: true,
+  },
+  {
+    id: 3,
+    name: "password",
+    placeholder: "Password *",
+    type: "password",
+    min: "6",
+    required: true,
+  },
+];
