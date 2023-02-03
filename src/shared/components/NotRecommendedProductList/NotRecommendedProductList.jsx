c650835dd3029f1c8e4ccd043f428c868958bed8
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-03 19:08:18 +0200

    Create NotRecommendedProductList.jsx

---
+++ src/shared/components/NotRecommendedProductList/NotRecommendedProductList.jsx
@@ -0,0 +1,21 @@
+import s from "./notRecommendedProductList.module.scss";
+
+function NotRecommendedProductList({ className, list }) {
+  let contentList = [];
+  let counter = 0;
+
+  while (counter < 6) {
+    contentList.push(list[Math.floor(Math.random() * 47)]);
+    counter += 1;
+  }
+  
+  return (
+    <div className={`${s} ${s.wrapper} ${className}`}>
+      <h3 className={s.title}>Food not recommended</h3>
+      <ol className={s.list}>   
+        {contentList.map((item, idx) => <li key={idx+1} className={s.item}> {item}</li>)}  
+      </ol>
+    </div>
+  );
+}
+export default NotRecommendedProductList;
