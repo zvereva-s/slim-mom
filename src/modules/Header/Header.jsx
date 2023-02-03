037b7c66c13e288658c30a8740b87fbd8c6f8709
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-03 19:04:38 +0200

    Create Header.jsx

---
+++ src/modules/Header/Header.jsx
@@ -0,0 +1,10 @@
+import useAuthState from "../../shared/hooks/useAuthState";
+
+import NotAuthHeader from "./NotAuthHeader";
+import AuthHeader from "./AuthHeader";
+
+function Header() {
+  const { isLogin } = useAuthState();
+  return <>{isLogin ? <AuthHeader /> : <NotAuthHeader />}</>;
+}
+export default Header;
