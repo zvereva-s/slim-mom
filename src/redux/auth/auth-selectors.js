d99fc7a9cedefbc4429cfe62c62e7bdee34ff367
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-03 20:47:48 +0200

    Update auth-selectors.js

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,4 +1,4 @@
 export const isLogin = ({ auth }) => auth.isLogin;
 export const auth = ({ auth }) => auth;
 export const user = ({ auth }) => auth.user;
-export const healthyData = ({ auth }) => auth.user?.healthyData;
+export const healthyData = ({ auth }) => auth.user.healthyData;
