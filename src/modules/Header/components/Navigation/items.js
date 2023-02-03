64653c8365de0a2cc4e79138deafe46bac9666f9
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-03 19:04:24 +0200

    Create items.js

---
+++ src/modules/Header/components/Navigation/items.js
@@ -0,0 +1,26 @@
+export const items = [
+  {
+    id: 1,
+    title: "Sign In",
+    link: "/signin",
+    private: false,
+  },
+  {
+    id: 2,
+    title: "Sign Up",
+    link: "/signup",
+    private: false,
+  },
+  {
+    id: 3,
+    title: "Diary",
+    link: "/diary",
+    private: true,
+  },
+  {
+    id: 4,
+    title: "Calculator",
+    link: "/calculator",
+    private: true,
+  },
+];
