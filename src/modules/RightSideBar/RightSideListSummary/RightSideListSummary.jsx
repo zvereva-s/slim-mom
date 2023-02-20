b22f499754bbd91f16e5a79adc4de3e0e59fe29b
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-20 16:45:47 +0200

    Update RightSideListSummary.jsx

--- src/modules/RightSideBar/RightSideListSummary/RightSideListSummary.jsx
+++ src/modules/RightSideBar/RightSideListSummary/RightSideListSummary.jsx
@@ -1,26 +1,40 @@
 import s from "./rightSideListSummary.module.scss";
 
-const date = "2022/09/29";
-function RightSideListSummary() {
+function RightSideListSummary({
+  dailyRate,
+  date,
+  left,
+  consumed,
+  procentOfDayNorm,
+  t,
+}) {
   return (
     <div className={s.wrapper}>
-      <h3 className={s.title}>Summary for {date}</h3>
+      <h3 className={s.title}>
+        {t.summaryTitle} {date}
+      </h3>
       <ul className={s.list}>
         <li className={s.item}>
-          <p className={s["item-title"]}>Left</p>
-          <p className={s.kcal}>000 kcal</p>
+          <p className={s["item-title"]}>{t.left}</p>
+          <p className={s.kcal}>
+            {left} {t.kcal}
+          </p>
         </li>
         <li className={s.item}>
-          <p className={s["item-title"]}>Consumed</p>
-          <p className={s.kcal}>000 kcal</p>
+          <p className={s["item-title"]}>{t.consumed}</p>
+          <p className={s.kcal}>
+            {consumed} {t.kcal}
+          </p>
         </li>
         <li className={s.item}>
-          <p className={s["item-title"]}>Daily rate</p>
-          <p className={s.kcal}>000 kcal</p>
+          <p className={s["item-title"]}>{t.dailyRate}</p>
+          <p className={s.kcal}>
+            {dailyRate} {t.kcal}
+          </p>
         </li>
         <li className={s.item}>
-          <p className={s["item-title"]}>n% of normal</p>
-          <p className={s.kcal}>000 kcal</p>
+          <p className={s["item-title"]}>{t.procentOfNorm}</p>
+          <p className={s.kcal}>{procentOfDayNorm}%</p>
         </li>
       </ul>
     </div>
