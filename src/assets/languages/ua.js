3a05108b72cbe41715e6f02ba6650d0caf63c773
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-20 16:48:17 +0200

    Create ua.js

---
+++ src/assets/languages/ua.js
@@ -0,0 +1,60 @@
+export const ua = {
+  kcal: "ккал",
+  //SIGN UP
+  signUpTitle: "Реєстрація",
+  name: "Імʼя *",
+  email: "Електронна пошта *",
+  password: "Пароль *",
+  signUpBtn: "Зареєструватися",
+
+  //SIGN IN
+  signInTitle: "Ввійти",
+  login: "Логін *",
+  signInBtn: "Ввійти",
+
+  //link Input
+  show: "Показати",
+  close: "Сховати",
+
+  //HEADER - AUTH
+  diary: "Щоденник",
+  calculator: "Калькулятор",
+  logOut: "Вийти",
+
+  //DAILY CALORIES INFO
+  recomendedDaily: "Ваша рекомендована денна норма",
+
+  //CALCULATOR
+  calculatorTitle: "Розрахуйте свою денну норму калорій прямо зараз",
+  height: "Зріст *",
+  age: "Вік *",
+  weight: "Вага *",
+  desiredWeight: "Бажана вага *",
+  bloodType: "Група крові *",
+  gender: "Стать *",
+  male: "чоловік",
+  female: "жінка",
+  physicalActivity: "Фізична активність *",
+  minimal: "мінімальна",
+  medium: "середня",
+  low: "низька",
+  high: "висока",
+  extreme: "дуже висока",
+  calculatorBtn: "Розпочни втрачати вагу",
+
+  //DIARY
+  //ADD FORM
+  addFormInput: "Введіть назву продукту",
+  inputWeight: "Грам",
+  g: "г",
+
+  //SUMMARY
+  summaryTitle: "Підсумки на ",
+  left: "Залишилось",
+  consumed: "Всього",
+  dailyRate: "Добова норма",
+  procentOfNorm: "% від норми",
+
+  //NOT RECOMMENDED
+  titleNotRecom: "Нерекомендована їжа",
+};
