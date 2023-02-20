f8cdfb47e7b1b2d967bec6965218faadce0aee2e
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-20 16:45:06 +0200

    Update DiaryProductsList.jsx

--- src/modules/Diary/DiaryProductsList/DiaryProductsList.jsx
+++ src/modules/Diary/DiaryProductsList/DiaryProductsList.jsx
@@ -1,25 +1,35 @@
+import { useDispatch } from "react-redux";
+import { deleteProductRequest } from "../../../redux/diary/diary-operations";
+
 import DiaryProductsListItem from "./DiaryProductsListItem/DiaryProductsListItem";
 
 import s from "./diaryProductsList.module.scss";
 
 // виводить по условию на последнем элементе див с тенью /
 
-function DiaryProductsList() {
+function DiaryProductsList({ products, t }) {
+  const dispatch = useDispatch();
+
+  function deleteProduct(id) {
+    dispatch(deleteProductRequest(id));
+  }
   return (
     <>
       <div className={s.wrapper}>
         <ul className={s.list}>
-          <DiaryProductsListItem />
-          <DiaryProductsListItem />
-          <DiaryProductsListItem />
-          <DiaryProductsListItem />
-          <DiaryProductsListItem />
-          <DiaryProductsListItem />
-          <DiaryProductsListItem />
-          <DiaryProductsListItem />
+          {products.map((product) => (
+            <DiaryProductsListItem
+              key={product._id}
+              name={product.name}
+              weight={product.weight}
+              sumCaloriesOfProduct={Math.round(product.sumCaloriesOfProduct)}
+              onClick={() => deleteProduct(product._id)}
+              t={t}
+            />
+          ))}
         </ul>
       </div>
-      <div className={s.shadow}></div>
+      {/* <div className={s.shadow}></div> */}
     </>
   );
 }
