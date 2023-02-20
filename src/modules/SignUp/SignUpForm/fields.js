4ed38266403076288d275d8cfb98943633bb0db5
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-20 16:45:54 +0200

    Update fields.js

--- src/modules/SignUp/SignUpForm/fields.js
+++ src/modules/SignUp/SignUpForm/fields.js
@@ -1,8 +1,12 @@
+import { en } from "../../../assets/languages/en";
+import { ua } from "../../../assets/languages/ua";
+import { ru } from "../../../assets/languages/ru";
+
 export const fields = [
   {
     id: 1,
     name: "name",
-    placeholder: "Name *",
+    placeholder: { en: en.name, ua: ua.name, ru: ru.name },
     type: "text",
     min: "2",
     required: true,
@@ -10,14 +14,14 @@ export const fields = [
   {
     id: 2,
     name: "email",
-    placeholder: "Login *",
+    placeholder: { en: en.email, ua: ua.email, ru: ru.email },
     type: "email",
     required: true,
   },
   {
     id: 3,
     name: "password",
-    placeholder: "Password *",
+    placeholder: { en: en.password, ua: ua.password, ru: ru.password },
     type: "password",
     min: "6",
     required: true,
