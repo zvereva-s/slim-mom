4cfe960b0b106831704be4c9eea3f4f32bf51747
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-20 16:45:34 +0200

    Update UserInfo.jsx

--- src/modules/Header/components/UserInfo/UserInfo.jsx
+++ src/modules/Header/components/UserInfo/UserInfo.jsx
@@ -1,12 +1,14 @@
 import { useDispatch } from "react-redux";
 import { logoutRequest } from "../../../../redux/auth/auth-operations";
 import useAuthState from "../../../../shared/hooks/useAuthState";
+import useTranslate from "../../../../shared/hooks/useTranslate";
 
 import s from "./userInfo.module.scss";
 
 function UserInfo() {
   const dispatch = useDispatch();
   const { user } = useAuthState();
+  const { t } = useTranslate();
 
   const logout = () => {
     dispatch(logoutRequest());
@@ -16,7 +18,7 @@ function UserInfo() {
     <div className={s.wrapper}>
       <p className={s["item"]}>{user.name}</p>
       <p className={s["item"]} onClick={logout}>
-        Log Out
+        {t.logOut}
       </p>
     </div>
   );
