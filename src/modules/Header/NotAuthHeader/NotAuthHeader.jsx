ed69d5d44934587e4307ee23cfbdbb31e7338d13
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-20 16:45:39 +0200

    Update NotAuthHeader.jsx

--- src/modules/Header/NotAuthHeader/NotAuthHeader.jsx
+++ src/modules/Header/NotAuthHeader/NotAuthHeader.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 
 import LangSwitcher from "../components/LangSwitcher";
+import ThemeSwitcher from "../../../shared/components/ThemeSwitcher/ThemeSwitcher";
 import Navigation from "../components/Navigation";
 
 import Logo from "../../../shared/components/Logo";
@@ -11,6 +12,7 @@ function NotAuthHeader() {
   return (
     <header className={s.header}>
       <LangSwitcher />
+      <ThemeSwitcher />
       <div className={s.container}>
         <Link to="/">
           <Logo />
