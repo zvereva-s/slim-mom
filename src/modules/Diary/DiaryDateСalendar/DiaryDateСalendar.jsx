dc39df605b72ef4b18cb3e813be836e2d6dee09c
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-20 16:45:01 +0200

    Update DiaryDateСalendar.jsx

--- src/modules/Diary/DiaryDateСalendar/DiaryDateСalendar.jsx
+++ src/modules/Diary/DiaryDateСalendar/DiaryDateСalendar.jsx
@@ -1,16 +1,26 @@
+import { useSelector } from "react-redux";
 import { HandySvg } from "handy-svg";
+
+import { diaryDate } from "../../../redux/diary/diary-selector";
+import Calendar from "../../../shared/components/Calendar/Calendar";
+
+import { converToDate } from "../../../shared/services/utils/utils";
 
 import s from "./diaryDateСalendar.module.scss";
 import icon from "../../../assets/images/Calendar/icon.svg";
 
-const data = "02.09.2022";
-function DiaryDateСalendar() {
+function DiaryDateСalendar({ onClick, openCalendar }) {
+  const dateValue = useSelector(diaryDate);
+  const { year, month, day } = converToDate(dateValue);
+  const date = `${day}.${month}.${year}`;
+
   return (
     <div className={s["main-wrapper"]}>
-      <h1 className={s.title}>{data}</h1>
-      <div className={s["icon-wrapper"]}>
+      <h1 className={s.title}>{date}</h1>
+      <div className={s["icon-wrapper"]} onClick={onClick}>
         <HandySvg className={s.icon} src={icon} />
       </div>
+      {openCalendar && <Calendar dateValue={dateValue} />}
     </div>
   );
 }
