e38400c2c66accab701b0c477baf8a3f4f695a7b
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-20 16:45:56 +0200

    Update SignUpForm.jsx

--- src/modules/SignUp/SignUpForm/SignUpForm.jsx
+++ src/modules/SignUp/SignUpForm/SignUpForm.jsx
@@ -1,4 +1,6 @@
 import classNames from "classnames";
+
+import useTranslate from "../../../shared/hooks/useTranslate";
 import useForm from "../../../shared/hooks/useForm";
 
 import FormField from "../../../shared/components/FormField";
@@ -11,25 +13,26 @@ import { fields } from "./fields";
 import s from "./signUpForm.module.scss";
 
 function SignUpForm({ onSubmit }) {
+  const { lang, t } = useTranslate();
   const { state, handleChange, handleSubmit, error, validate } = useForm({
     onSubmit,
     initialState,
   });
 
   const textFields = fields.map((el) => (
-      <FormField
-        key={el.id}
-        name={el.name}
-        value={state[el.name]}
-        placeholder={el.placeholder}
-        type={el.type}
-        handleChange={handleChange}
-        handleBlur={validate}
-        error={error}
-        min={el.min}
-        link={ el.type === 'password' && true}
-        required
-      />
+    <FormField
+      key={el.id}
+      name={el.name}
+      value={state[el.name]}
+      placeholder={el.placeholder[lang]}
+      type={el.type}
+      handleChange={handleChange}
+      handleBlur={validate}
+      error={error}
+      min={el.min}
+      link={el.type === "password" && true}
+      required
+    />
   ));
 
   return (
@@ -38,12 +41,12 @@ function SignUpForm({ onSubmit }) {
       <AuthSocial />
       <div className={s[`wrapper-btn`]}>
         <Button
-          text="Sign Up"
+          text={t.signUpBtn}
           type="submit"
           className={classNames("button", "text", "focus")}
         />
         <Button
-          text="Sign In"
+          text={t.signInBtn}
           type="submit"
           className={classNames("button", "text", "disabled")}
           disabled
