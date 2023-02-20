02f6f623db5a8f37bd765143ee8b440ba4710356
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-20 16:48:14 +0200

    Create ru.js

---
+++ src/assets/languages/ru.js
@@ -0,0 +1,60 @@
+export const ru = {
+  kcal: "ккал",
+  //SIGN UP
+  signUpTitle: "Регистрация",
+  name: "Имя *",
+  email: "Электронная почта *",
+  password: "Пароль *",
+  signUpBtn: "Зарегистрироваться",
+
+  //SIGN IN
+  signInTitle: "Войти",
+  login: "Логин *",
+  signInBtn: "Войти",
+
+  //link Input
+  show: "Показать",
+  close: "Спрятать",
+
+  //HEADER - AUTH
+  diary: "Дневник",
+  calculator: "Калькулятор",
+  logOut: "Выйти",
+
+  //DAILY CALORIES INFO
+  recomendedDaily: "Ваша рекомендуемая дневная норма",
+
+  //CALCULATOR
+  calculatorTitle: "Рассчитайте свою суточную калорийность прямо сейчас",
+  height: "Рост *",
+  age: "Возраст *",
+  weight: "Вес *",
+  desiredWeight: "Желаемый вес *",
+  bloodType: "Группа крови *",
+  gender: "Пол *",
+  male: "мужчина",
+  female: "женщина",
+  physicalActivity: "Физическая активность *",
+  minimal: "минимальная",
+  medium: "средняя",
+  low: "низкая",
+  high: "высокая",
+  extreme: "очень высокая",
+  calculatorBtn: "Начни терять вес",
+
+  //DIARY
+  //ADD FORM
+  addFormInput: "Введите название продукта",
+  inputWeight: "Грамм",
+  g: "g",
+
+  //SUMMARY
+  summaryTitle: "Итоги на ",
+  left: "Осталось",
+  consumed: "Всего",
+  dailyRate: "Дневная норма",
+  procentOfNorm: "% от нормы",
+
+  //NOT RECOMMENDED
+  titleNotRecom: "Нерекомендуемые продукты",
+};
