6479e4ea5effdd5c4f96e867dd4506b44e6947fd
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-20 16:47:54 +0200

    Update NotRecommendedProductList.jsx

--- src/shared/components/NotRecommendedProductList/NotRecommendedProductList.jsx
+++ src/shared/components/NotRecommendedProductList/NotRecommendedProductList.jsx
@@ -1,19 +1,28 @@
+import useTranslate from "../../hooks/useTranslate";
 import s from "./notRecommendedProductList.module.scss";
 
 function NotRecommendedProductList({ className, list }) {
+  const { t } = useTranslate();
   let contentList = [];
   let counter = 0;
 
-  while (counter < 5) {
-    contentList.push(list[Math.floor(Math.random() * 47)]);
-    counter += 1;
+  if (list) {
+    while (counter < 5) {
+      contentList.push(list[Math.floor(Math.random() * (47 - 1) + 1)]);
+      counter += 1;
+    }
   }
-  
+
   return (
     <div className={`${s} ${s.wrapper} ${className}`}>
-      <h3 className={s.title}>Food not recommended</h3>
-      <ol className={s.list}>   
-        {contentList.map((item, idx) => <li key={idx+1} className={s.item}> {item}</li>)}  
+      <h3 className={s.title}>{t.titleNotRecom}</h3>
+      <ol className={s.list}>
+        {contentList.map((item, idx) => (
+          <li key={idx + 1} className={s.item}>
+            {" "}
+            {item}
+          </li>
+        ))}
       </ol>
     </div>
   );
