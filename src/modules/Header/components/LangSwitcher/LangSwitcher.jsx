a75d80e3a5207f66f2e4160fcc838b474b0cc06f
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-20 16:45:20 +0200

    Update LangSwitcher.jsx

--- src/modules/Header/components/LangSwitcher/LangSwitcher.jsx
+++ src/modules/Header/components/LangSwitcher/LangSwitcher.jsx
@@ -1,11 +1,46 @@
+import classNames from "classnames";
+
+import useTranslate from "../../../../shared/hooks/useTranslate";
+import useTheme from "../../../../shared/hooks/useTheme";
+
 import s from "./langSwitcher.module.scss";
 
 function LangSwitcher() {
+  const { lang, setLangSchema } = useTranslate();
+  const { theme } = useTheme();
+
   return (
-    <div className={s["lang-box"]}>
-      <p className={s.lang}>EN</p>
-      <p className={s.lang}>|</p>
-      <p className={s.lang}>UA</p>
+    <div className={classNames(s["lang-box"], s[`lang-box-${theme}`])}>
+      <p
+        className={classNames(
+          s.lang,
+          s[`lang-${theme}`],
+          s[`en-${lang}-${theme}`]
+        )}
+        onClick={(e) => setLangSchema(e)}
+      >
+        EN
+      </p>
+      <p
+        className={classNames(
+          s.lang,
+          s[`lang-${theme}`],
+          s[`ua-${lang}-${theme}`]
+        )}
+        onClick={(e) => setLangSchema(e)}
+      >
+        UA
+      </p>
+      <p
+        className={classNames(
+          s.lang,
+          s[`lang-${theme}`],
+          s[`ru-${lang}-${theme}`]
+        )}
+        onClick={(e) => setLangSchema(e)}
+      >
+        RU
+      </p>
     </div>
   );
 }
