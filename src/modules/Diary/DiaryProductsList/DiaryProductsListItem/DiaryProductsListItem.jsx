906645585a264731f8dd1fd967a4f5ab3a3b3a64
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-20 16:45:10 +0200

    Update DiaryProductsListItem.jsx

--- src/modules/Diary/DiaryProductsList/DiaryProductsListItem/DiaryProductsListItem.jsx
+++ src/modules/Diary/DiaryProductsList/DiaryProductsListItem/DiaryProductsListItem.jsx
@@ -4,15 +4,29 @@ import Button from "../../../../shared/components/Button";
 
 import s from "./diaryProductsListItem.module.scss";
 
-function DiaryProductsListItem() {
+function DiaryProductsListItem({
+  name,
+  weight,
+  sumCaloriesOfProduct,
+  onClick,
+  t,
+}) {
   return (
     <li className={s.item}>
-      <p className={classNames(s.product, s.text)}>Name</p>
-      <p className={classNames(s.weight, s.text)}>100 g</p>
-      <p className={classNames(s.kcal, s.text)}>
-        100 <span className={s["kcal-text"]}>kcal</span>
+      <p className={classNames(s.product, s.text)}>{name}</p>
+      <p className={classNames(s.weight, s.text)}>
+        {weight}
+        {t.g}
       </p>
-      <Button type="button" className={classNames("button")} icon="close" />
+      <p className={classNames(s.kcal, s.text)}>
+        {sumCaloriesOfProduct} <span className={s["kcal-text"]}>{t.kcal}</span>
+      </p>
+      <Button
+        type="button"
+        className={classNames("button")}
+        icon="close"
+        onClick={onClick}
+      />
     </li>
   );
 }
