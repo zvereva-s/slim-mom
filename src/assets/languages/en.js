ce060f91da149a7e683675c0c80756189524344b
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-20 16:48:12 +0200

    Create en.js

---
+++ src/assets/languages/en.js
@@ -0,0 +1,60 @@
+export const en = {
+  kcal: "kcal",
+  //SIGN UP
+  signUpTitle: "Sing up",
+  name: "Name *",
+  email: "Email *",
+  password: "Password *",
+  signUpBtn: "Sign up",
+
+  //SIGN IN
+  signInTitle: "Sign In",
+  login: "Login *",
+  signInBtn: "Sign In",
+
+  //link Input
+  show: "Show",
+  close: "Close",
+
+  //HEADER - AUTH
+  diary: "Diary",
+  calculator: "Calculator",
+  logOut: "Log Out",
+
+  //CALCULATOR
+  calculatorTitle: "Calculate your daily calorie intake right now",
+  height: "Height *",
+  age: "Age *",
+  weight: "Weight *",
+  desiredWeight: "Desired Weight *",
+  bloodType: "BloodType *",
+  gender: "Gender *",
+  male: "male",
+  female: "female",
+  physicalActivity: "Physical Activity *",
+  minimal: "minimal",
+  medium: "medium",
+  low: "low",
+  high: "high",
+  extreme: "extreme",
+  calculatorBtn: "Start losing weight",
+
+  //DAILY CALORIES INFO
+  recomendedDaily: "Your recommended daily calorie intake is",
+
+  //DIARY
+  //ADD FORM
+  addFormInput: "Type name of the product",
+  inputWeight: "Grams",
+  g: "г",
+
+  //SUMMARY
+  summaryTitle: "Summary for",
+  left: "Left",
+  consumed: "Consumed",
+  dailyRate: "Daily rate",
+  procentOfNorm: "n% of normal",
+
+  //NOT RECOMMENDED
+  titleNotRecom: "Food not recommended",
+};
