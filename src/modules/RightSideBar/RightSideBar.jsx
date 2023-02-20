c202a56a9b51fff50fadad84fd2eaf86e28f8b48
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-20 16:45:44 +0200

    Update RightSideBar.jsx

--- src/modules/RightSideBar/RightSideBar.jsx
+++ src/modules/RightSideBar/RightSideBar.jsx
@@ -1,23 +1,55 @@
-import useAuthState from "../../shared/hooks/useAuthState";
+import { useSelector, useDispatch } from "react-redux";
+
+import { summaryOfDayRequest } from "../../redux/healthyData/healthyData-operations";
+
+import {
+  healthyDataObj,
+  summaryObj,
+} from "../../redux/healthyData/healthyData-selectors";
+import { diaryDate } from "../../redux/diary/diary-selector";
+
+import { converToDate } from "../../shared/services/utils/utils";
 
 import RightSideListSummary from "./RightSideListSummary";
 import NotRecommendedProductList from "../../shared/components/NotRecommendedProductList";
 
+import useTranslate from "../../shared/hooks/useTranslate";
+
 import s from "./rightSideBar.module.scss";
+import { useEffect } from "react";
 
 function RightSideBar() {
-   //! lang
-  const lang = 'en';
-  //! 
+  const { lang, t } = useTranslate();
+  const dispatch = useDispatch();
 
-  const { user } = useAuthState();
-  const { healthyData } = user;
-  const { notAllowedProducts } = healthyData;
+  const dateValue = useSelector(diaryDate);
+  const { year, month, day } = converToDate(dateValue);
+
+  const date = `${year}/${month}/${day}`;
+
+  const { healthyData } = useSelector(healthyDataObj);
+  const { notAllowedProducts, dailyRate } = healthyData;
+
+  const { left, consumed, procentOfDayNorm } = useSelector(summaryObj);
+
+  useEffect(() => {
+    dispatch(summaryOfDayRequest(`${day}.${month}.${year}`));
+  }, []);
 
   return (
     <div className={s.wrapper}>
-      <RightSideListSummary />
-     <NotRecommendedProductList className={s.notRecommend} list={notAllowedProducts[lang]} />
+      <RightSideListSummary
+        dailyRate={dailyRate}
+        date={date}
+        left={left}
+        consumed={consumed}
+        procentOfDayNorm={procentOfDayNorm}
+        t={t}
+      />
+      <NotRecommendedProductList
+        className={s.notRecommend}
+        list={notAllowedProducts?.[lang]}
+      />
     </div>
   );
 }
