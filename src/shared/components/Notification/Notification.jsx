0e650a39cb44bb7d2932b220dc9ea76ebc851683
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-22 21:16:24 +0200

    Update Notification.jsx

--- src/shared/components/Notification/Notification.jsx
+++ src/shared/components/Notification/Notification.jsx
@@ -1,12 +1,19 @@
+import classNames from "classnames";
 
-import s from './notification.module.scss'
+import useTheme from "../../hooks/useTheme";
+
+import s from "./notification.module.scss";
 
 function Notification({ text, onClick }) {
+  const { theme } = useTheme();
   return (
-    <div onClick={onClick} className={s.wrapper}>
-      <p className={s.text}>{text}</p>
+    <div
+      onClick={onClick}
+      className={classNames(s.wrapper, s[`wrapper-${theme}`])}
+    >
+      <p className={classNames(s.text, s[`text-${theme}`])}>{text}</p>
     </div>
-  )
+  );
 }
 
 export default Notification;
