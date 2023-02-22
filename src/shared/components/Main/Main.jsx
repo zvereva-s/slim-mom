2818c6ddebbf18958c7231adfc4877cc6497470b
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-22 21:16:20 +0200

    Update Main.jsx

--- src/shared/components/Main/Main.jsx
+++ src/shared/components/Main/Main.jsx
@@ -1,11 +1,12 @@
 import { useMediaPredicate } from "react-media-hook";
+import useAuthState from "../../hooks/useAuthState";
 import s from "./main.module.scss";
 
-const isAuth = false;
-
 function Main({ children }) {
+  const { isLogin } = useAuthState();
+
   const biggerThan1280 = useMediaPredicate("(min-width: 1280px)");
-  const style = isAuth && biggerThan1280 ? s["main-flex"] : "";
+  const style = isLogin && biggerThan1280 ? s["main-flex"] : "";
   return <main className={style}>{children}</main>;
 }
 export default Main;
