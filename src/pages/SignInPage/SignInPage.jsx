9599903c19bbea465a3610e3834e2e733ad2b1f7
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-22 21:15:00 +0200

    Update SignInPage.jsx

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,4 +1,7 @@
+import classNames from "classnames";
+
 import useTranslate from "../../shared/hooks/useTranslate";
+import useTheme from "../../shared/hooks/useTheme";
 
 import SignIn from "../../modules/SignIn";
 
@@ -11,11 +14,16 @@ import s from "./signInPage.module.scss";
 
 function SignInPage() {
   const { t } = useTranslate();
+  const { theme } = useTheme();
+
   return (
     <Main>
       <Section>
         <Container>
-          <Title text={t.signInTitle} className={s.title} />
+          <Title
+            text={t.signInTitle}
+            className={classNames(s.title, s[`title-${theme}`])}
+          />
           <SignIn />
         </Container>
       </Section>
