449d01ebf1c02e9c185dcc77897f8b8b55ab2319
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-22 21:16:36 +0200

    Update Title.jsx

--- src/shared/components/Title/Title.jsx
+++ src/shared/components/Title/Title.jsx
@@ -1,7 +1,15 @@
+import useTheme from "../../../shared/hooks/useTheme";
+
 import s from "./title.module.scss";
 
 function Title({ text, className }) {
-  return <h1 className={`${s} ${s.title} ${className}`}>{text}</h1>;
+  const { theme } = useTheme();
+
+  return (
+    <h1 className={`${s} ${s.title} ${className} ${s[`title-${theme}`]}`}>
+      {text}
+    </h1>
+  );
 }
 
 export default Title;
