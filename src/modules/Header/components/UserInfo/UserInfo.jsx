2b2f19a01163fd1cef4d456fba8f5db78c0774fb
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-22 21:14:27 +0200

    Update UserInfo.jsx

--- src/modules/Header/components/UserInfo/UserInfo.jsx
+++ src/modules/Header/components/UserInfo/UserInfo.jsx
@@ -1,7 +1,12 @@
+import classNames from "classnames";
+
 import { useDispatch } from "react-redux";
+
 import { logoutRequest } from "../../../../redux/auth/auth-operations";
+
 import useAuthState from "../../../../shared/hooks/useAuthState";
 import useTranslate from "../../../../shared/hooks/useTranslate";
+import useTheme from "../../../../shared/hooks/useTheme";
 
 import s from "./userInfo.module.scss";
 
@@ -9,15 +14,16 @@ function UserInfo() {
   const dispatch = useDispatch();
   const { user } = useAuthState();
   const { t } = useTranslate();
+  const { theme } = useTheme();
 
   const logout = () => {
     dispatch(logoutRequest());
   };
 
   return (
-    <div className={s.wrapper}>
-      <p className={s["item"]}>{user.name}</p>
-      <p className={s["item"]} onClick={logout}>
+    <div className={classNames(s.wrapper, s[`wrapper-${theme}`])}>
+      <p className={classNames(s.item, s[`item-${theme}`])}>{user.name}</p>
+      <p className={classNames(s.item, s[`item-${theme}`])} onClick={logout}>
         {t.logOut}
       </p>
     </div>
