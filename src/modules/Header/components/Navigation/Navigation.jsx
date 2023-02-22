f4b66801840f46fb383764133d306ed9a8669414
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-22 21:14:20 +0200

    Update Navigation.jsx

--- src/modules/Header/components/Navigation/Navigation.jsx
+++ src/modules/Header/components/Navigation/Navigation.jsx
@@ -1,27 +1,34 @@
+import classNames from "classnames";
 import { NavLink } from "react-router-dom";
 
 import useAuthState from "../../../../shared/hooks/useAuthState";
 import useTranslate from "../../../../shared/hooks/useTranslate";
+import useTheme from "../../../../shared/hooks/useTheme";
 
 import s from "./navigation.module.scss";
 import { items } from "./items";
 
-function getClassName({ isActive }) {
-  const style = isActive ? s.isActive : s.link;
-  return style;
-}
-
 function Navigation({ isModalOpen, closeModal, children }) {
+  const { theme } = useTheme();
   const { lang } = useTranslate();
   const { isLogin } = useAuthState();
 
-  const styleNav = isLogin ? s["nav-isLogin"] : s.nav;
+  function getClassName({ isActive }) {
+    const style = isActive
+      ? classNames(s.isActive, s[`isActive-${theme}`])
+      : classNames(s.link, s[`link-${theme}`]);
+    return style;
+  }
+
+  const styleNav = isLogin
+    ? classNames(s["nav-isLogin"], s[`nav-isLogin-${theme}`])
+    : classNames(s.nav, s[`nav-${theme}`]);
   const menu = isLogin
     ? items.filter((el) => el.private === true)
     : items.filter((el) => el.private === false);
 
   const elements = menu.map(({ id, link, title }) => (
-    <li key={id} className={s.item}>
+    <li key={id} className={classNames(s.item, s[`item-${theme}`])}>
       <NavLink className={getClassName} to={link} onClick={closeModal}>
         {title[lang]}
       </NavLink>
@@ -31,12 +38,12 @@ function Navigation({ isModalOpen, closeModal, children }) {
   return (
     <>
       <nav className={styleNav}>
-        <ul className={s.list}>{elements}</ul>
+        <ul className={classNames(s.list, s[`list-${theme}`])}>{elements}</ul>
         {children}
       </nav>
       {isModalOpen && (
-        <nav className={s["nav-modal"]}>
-          <ul className={s.list}>{elements}</ul>
+        <nav className={classNames(s["nav-modal"], s[`nav-modal-${theme}`])}>
+          <ul className={classNames(s.list, s[`list-${theme}`])}>{elements}</ul>
           {children}
         </nav>
       )}
