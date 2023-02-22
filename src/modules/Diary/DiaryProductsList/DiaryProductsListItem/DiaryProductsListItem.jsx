9279c49c2c2cebdabf8404d46152601949e86da7
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-22 21:13:49 +0200

    Update DiaryProductsListItem.jsx

--- src/modules/Diary/DiaryProductsList/DiaryProductsListItem/DiaryProductsListItem.jsx
+++ src/modules/Diary/DiaryProductsList/DiaryProductsListItem/DiaryProductsListItem.jsx
@@ -1,5 +1,7 @@
 import classNames from "classnames";
 
+import useTheme from "../../../../shared/hooks/useTheme";
+
 import Button from "../../../../shared/components/Button";
 
 import s from "./diaryProductsListItem.module.scss";
@@ -11,19 +13,46 @@ function DiaryProductsListItem({
   onClick,
   t,
 }) {
+  const { theme } = useTheme();
   return (
-    <li className={s.item}>
-      <p className={classNames(s.product, s.text)}>{name}</p>
-      <p className={classNames(s.weight, s.text)}>
+    <li className={classNames(s.item, s[`item-${theme}`])}>
+      <p
+        className={classNames(
+          s.product,
+          s.text,
+          s[`product-${theme}`],
+          s[`text-${theme}`]
+        )}
+      >
+        {name}
+      </p>
+      <p
+        className={classNames(
+          s.weight,
+          s.text,
+          s[`weight-${theme}`],
+          s[`text-${theme}`]
+        )}
+      >
         {weight}
         {t.g}
       </p>
-      <p className={classNames(s.kcal, s.text)}>
-        {sumCaloriesOfProduct} <span className={s["kcal-text"]}>{t.kcal}</span>
+      <p
+        className={classNames(
+          s.kcal,
+          s.text,
+          s[`kcal-${theme}`],
+          s[`text-${theme}`]
+        )}
+      >
+        {sumCaloriesOfProduct}{" "}
+        <span className={classNames(s["kcal-text"], s[`kcal-text-${theme}`])}>
+          {t.kcal}
+        </span>
       </p>
       <Button
         type="button"
-        className={classNames("button")}
+        className={classNames("button", `button-${theme}`)}
         icon="close"
         onClick={onClick}
       />
