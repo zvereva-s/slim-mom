74a574ea6f4dfeba7d3e5b9954dccbf4156d2909
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-22 21:16:15 +0200

    Update Logo.jsx

--- src/shared/components/Logo/Logo.jsx
+++ src/shared/components/Logo/Logo.jsx
@@ -1,16 +1,25 @@
 import { HandySvg } from "handy-svg";
+import classNames from "classnames";
+
+import useAuthState from "../../hooks/useAuthState";
+import useTheme from "../../hooks/useTheme";
 
 import icon from "../../../assets/images/Logo/Logo.svg";
 import text from "../../../assets/images/Logo/Text.svg";
+
 import s from "./logo.module.scss";
 
-const isLogin = false;
-const style = isLogin ? s["text-auth"] : s["text"];
-
 function Logo({ isModalOpen }) {
+  const { theme } = useTheme();
+  const { isLogin } = useAuthState();
+
+  const style = isLogin
+    ? s[`text-${theme}-auth`]
+    : classNames(s["text"], s[`text-${theme}`]);
+
   return (
-    <div className={s.wrapper}>
-      <HandySvg className={s.icon} src={icon} />
+    <div className={classNames(s.wrapper, s[`wrapper-${theme}`])}>
+      <HandySvg className={classNames(s.icon, s[`icon-${theme}`])} src={icon} />
       <HandySvg className={style} src={text} />
       {isModalOpen && <HandySvg className={s["text-logo-modal"]} src={text} />}
     </div>
