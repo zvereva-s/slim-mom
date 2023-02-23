2f327d4f7b7844db7d61991764eb28d055092809
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-23 21:25:32 +0200

    Update RightSideListSummary.jsx

--- src/modules/RightSideBar/RightSideListSummary/RightSideListSummary.jsx
+++ src/modules/RightSideBar/RightSideListSummary/RightSideListSummary.jsx
@@ -16,24 +16,38 @@ function RightSideListSummary({
   return (
     <div className={s.wrapper}>
       <h3 className={classNames(s.title, s[`title-${theme}`])}>
-        {t.summaryTitle} {date}
+        {`${t.summaryTitle}  ${date}`}
       </h3>
-      <ul className={s.list}>
+      <ul className={classNames(s.list, s[`list-${theme}`])}>
         <li className={s.item}>
           <p className={s["item-title"]}>{t.left}</p>
-          <p className={s.kcal}>
+          <p
+            className={s.kcal}
+            style={{
+              color: left < 0 && "red",
+            }}
+          >
             {left} {t.kcal}
           </p>
         </li>
         <li className={s.item}>
           <p className={s["item-title"]}>{t.consumed}</p>
-          <p className={s.kcal}>
+          <p
+            className={s.kcal}
+            style={{
+              color:
+                ((consumed > dailyRate && "red") ||
+                  (consumed < dailyRate && "orange")) &&
+                (theme === "light" ? "#07aa07" : "#c5ff87"),
+              fontWeight: 800,
+            }}
+          >
             {consumed} {t.kcal}
           </p>
         </li>
         <li className={s.item}>
           <p className={s["item-title"]}>{t.dailyRate}</p>
-          <p className={s.kcal}>
+          <p className={s.kcal} style={{ color: dailyRate === "0" && "red" }}>
             {dailyRate} {t.kcal}
           </p>
         </li>
