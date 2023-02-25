3c0423692ddf47495545126e603940fb503668a6
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-25 12:37:53 +0200

    removed env

--- src/shared/services/apis/api.js
+++ src/shared/services/apis/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-import { API_URL } from "@env";
 
 export const instanceClear = axios.create({
-  baseURL: API_URL,
+  baseURL: "https://slim-mom-backend.onrender.com",
 });
