8022aa87d25cd44628628005bec042b520a3de36
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-27 21:59:42 +0200

    Update api.js
    Change the baseURL

--- src/shared/services/apis/api.js
+++ src/shared/services/apis/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const instanceClear = axios.create({
-  baseURL: "https://slim-mom-backend.onrender.com",
+  baseURL: "https://slim-mom-backend.onrender.com/api",
 });
