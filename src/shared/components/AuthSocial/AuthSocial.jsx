6b8b289c577385f8df1a59c32137e364e50d4030
sophia zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-02-28 15:41:04 +0200

    Update AuthSocial.jsx

--- src/shared/components/AuthSocial/AuthSocial.jsx
+++ src/shared/components/AuthSocial/AuthSocial.jsx
@@ -7,14 +7,13 @@ import s from "./authSocial.module.scss";
 function AuthSocial() {
   return (
     <div className={s.wrapper}>
-      {/* <Button type='button' icon="fb" className={classNames("button")}/> */}
       <Button
         type="button"
         icon="google"
         className={classNames("button")}
         onClick={() => {
           window.location.replace(
-            "https://slim-mom-backend.onrender.com/api/auth/google"
+            "https://slim-mom-backend.onrender.com/api/auth/google/"
           );
         }}
       />
