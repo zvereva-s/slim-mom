8a11caa73679f61cea76005e5259ad66298abece
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-03-01 17:47:35 +0200

    add React StrictMode

--- src/index.js
+++ src/index.js
@@ -13,17 +13,17 @@ import { store, persistor } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter basename="https://slim-mom-static.onrender.com">
-    <Provider store={store}>
-      <LanguageProvider>
-        <ThemeProvider>
-          <PersistGate loading={null} persistor={persistor}>
-            <App />
-          </PersistGate>
-        </ThemeProvider>
-      </LanguageProvider>
-    </Provider>
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="https://slim-mom-static.onrender.com">
+      <Provider store={store}>
+        <LanguageProvider>
+          <ThemeProvider>
+            <PersistGate loading={null} persistor={persistor}>
+              <App />
+            </PersistGate>
+          </ThemeProvider>
+        </LanguageProvider>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
