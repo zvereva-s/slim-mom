17f13ee698adfe1154058b59a68bd3a451356883
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-03-02 18:24:13 +0200

    delete react strictMode

--- src/index.js
+++ src/index.js
@@ -13,17 +13,17 @@ import { store, persistor } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <LanguageProvider>
-          <ThemeProvider>
-            <PersistGate loading={null} persistor={persistor}>
-              <App />
-            </PersistGate>
-          </ThemeProvider>
-        </LanguageProvider>
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <LanguageProvider>
+        <ThemeProvider>
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
+        </ThemeProvider>
+      </LanguageProvider>
+    </Provider>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
